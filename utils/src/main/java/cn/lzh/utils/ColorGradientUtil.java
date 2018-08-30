106e21765127d2449f5773155374bbeddff6850c
zhuanglee <devlzh93@gmail.com>
2018-08-30 17:56:41 +0800

    优化

--- utils/src/main/java/cn/lzh/utils/ColorGradientUtil.java
+++ utils/src/main/java/cn/lzh/utils/ColorGradientUtil.java
@@ -12,23 +12,23 @@ public class ColorGradientUtil {
      * 计算从startColor过度到endColor过程中百分比为franch时的颜色值
      * @param startColor 起始颜色 int类型
      * @param endColor 结束颜色 int类型
-     * @param rate 百分比0.5
+     * @param ratio 百分比0.5
      * @return 返回int格式的color
      */
-    public static int getColor(int startColor, int endColor, float rate){
+    public static int getColor(int startColor, int endColor, float ratio){
         String strStartColor = "#" + Integer.toHexString(startColor);
         String strEndColor = "#" + Integer.toHexString(endColor);
-        return Color.parseColor(getColor(strStartColor, strEndColor, rate));
+        return Color.parseColor(getColor(strStartColor, strEndColor, ratio));
     }
 
     /**
      * 计算从startColor过度到endColor过程中百分比为franch时的颜色值
      * @param startColor 起始颜色 （格式#FFFFFFFF）
      * @param endColor 结束颜色 （格式#FFFFFFFF）
-     * @param rate 百分比0.5
+     * @param ratio 百分比0.5
      * @return 返回String格式的color（格式#FFFFFFFF）
      */
-    public static String getColor(String startColor, String endColor, float rate){
+    public static String getColor(String startColor, String endColor, float ratio){
 
         int startAlpha = Integer.parseInt(startColor.substring(1, 3), 16);
         int startRed = Integer.parseInt(startColor.substring(3, 5), 16);
@@ -40,10 +40,10 @@ public class ColorGradientUtil {
         int endGreen = Integer.parseInt(endColor.substring(5, 7), 16);
         int endBlue = Integer.parseInt(endColor.substring(7), 16);
 
-        int currentAlpha = (int) ((endAlpha - startAlpha) * rate + startAlpha);
-        int currentRed = (int) ((endRed - startRed) * rate + startRed);
-        int currentGreen = (int) ((endGreen - startGreen) * rate + startGreen);
-        int currentBlue = (int) ((endBlue - startBlue) * rate + startBlue);
+        int currentAlpha = (int) ((endAlpha - startAlpha) * ratio + startAlpha);
+        int currentRed = (int) ((endRed - startRed) * ratio + startRed);
+        int currentGreen = (int) ((endGreen - startGreen) * ratio + startGreen);
+        int currentBlue = (int) ((endBlue - startBlue) * ratio + startBlue);
 
         return "#" + getHexString(currentAlpha) + getHexString(currentRed)
                 + getHexString(currentGreen) + getHexString(currentBlue);
